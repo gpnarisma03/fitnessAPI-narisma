d632a11c7e6474aedc4ac21fc34f10b8e740d936
gpnarisma03 <narismagerald@gmail.com>
2025-05-07 15:18:22 +0800

    fix

--- controllers/workout.js
+++ controllers/workout.js
@@ -36,7 +36,7 @@ exports.addWorkOut = async (req, res) => {
   
       const workouts = await Workout.find({ userId }).sort({ dateAdded: -1 }); // latest first
   
-      res.status(200).json(workouts);
+      res.status(200).json({ workouts });
     } catch (error) {
       console.error('Error fetching workouts:', error.message);
       res.status(500).json({ message: 'Server error while fetching workouts.' });
@@ -69,7 +69,7 @@ exports.addWorkOut = async (req, res) => {
       });
     } catch (error) {
       console.error('Error updating workout:', error.message);
-      res.status(500).json({ message: 'Server error while updating workout.' });
+      res.status(500).json({ message: 'Server error while updating workout' });
     }
   };
 
@@ -87,7 +87,7 @@ exports.addWorkOut = async (req, res) => {
         return res.status(404).json({ message: 'Workout not found or unauthorized.' });
       }
   
-      res.status(200).json({ message: 'Workout deleted successfully.' });
+      res.status(200).json({ message: 'Workout deleted successfully' });
     } catch (error) {
       console.error('Error deleting workout:', error.message);
       res.status(500).json({ message: 'Server error while deleting workout.' });
@@ -102,10 +102,7 @@ exports.addWorkOut = async (req, res) => {
       // Update status field only
       const { status } = req.body;  // Assuming status can be 'completed', 'pending', etc.
   
-      if (status !== 'pending' && status !== 'completed') {
-        return res.status(400).json({ message: 'Invalid status value. Only "pending" or "completed" allowed.' });
-      }
-  
+
       const updatedWorkout = await Workout.findOneAndUpdate(
         { _id: workoutId, userId },
         { status },
